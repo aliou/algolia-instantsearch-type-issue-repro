9d15eb07a9bc4bbf7a2b5ff4fd40647ce2d82aa0
Aliou Diallo <code@aliou.me>
2023-10-09 09:34:16 +0200

    Reproduce issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { InstantSearch } from 'react-instantsearch-core';
+import { algoliasearch } from 'algoliasearch';
+import { Box } from '@chakra-ui/react';
+
+const searchClient = algoliasearch('', '');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box>
+        <InstantSearch searchClient={searchClient} indexName=''>
+          <div>hello</div>
+        </InstantSearch>
+      </Box>
+    </>
   );
 }
 
